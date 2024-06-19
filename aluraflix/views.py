dbf88631a1e9f96c1c3e7bbf1cfc90a8d127d213
wesnds <wesndsantos@gmail.com>
2024-06-19 16:15:58 -0400

    (django-back) Aprofundamento em estes e documentação [tipos de teste]     - carregando dados iniciais com loaddata através de fixture

--- aluraflix/views.py
+++ aluraflix/views.py
@@ -5,13 +5,12 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+
 class ProgramaViewSet(viewsets.ModelViewSet):
     queryset = Programa.objects.all()
-    # print(str(queryset.query))
     serializer_class = ProgramaSerializer
     filter_backends = [DjangoFilterBackend, filters.SearchFilter]
-    search_fields = ['titulo']
-    filterset_fields = ['tipo']
+    search_fields = ["titulo"]
+    filterset_fields = ["tipo"]
     authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
-
